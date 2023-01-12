22729c777f75e80740aaa3f08dd9651b1b0e7209
Anirudha Gupta <anirudhagupta01@gmail.com>
2023-01-12 11:56:09 +0530

    Add project files.

---
+++ AddTwoIntegersCpp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    sol.sum(1, 99);
+    std::cout << "Hello World!\n";
+}
